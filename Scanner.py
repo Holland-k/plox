ed4a41d6b06876780287885a91c02dca6cce8a1f
Holland <holland@hollandk.com>
2019-09-10 08:58:41 -0400

    fleshing out the scanner

--- Scanner.py
+++ Scanner.py
@@ -35,12 +35,59 @@ class Scanner:
 			addToken(SEMICOLON)
 		elif(c == '*'):
 			addToken(STAR)
-		else:
+                elif(c == '!'):
+                        if(match('=')):
+                                addToken(BANG_EQUAL)
+                        else:
+                                addToken(BANG)
+                elif(c == '='):
+                        if(match('=')):
+                                addToken(EQUAL_EQUAL)
+                        else:
+                                addToken(EQUAL)
+                elif(c == '<'):
+                        if(match('=')):
+                                addToken(LESS_EQUAL)
+                        else:
+                                addToken(LESS)
+                elif(c == '>'):
+                        if(match('=')):
+                                addToken(GREATER_EQUAL)
+                        else:
+                                addToken(GREATER)
+                elif(c == '/'):
+                        if(match('/')):
+                                while((peek() != '\n') and (not isAtEnd())):
+                                      advance()
+                        else:
+                                addToken(SLASH)
+                elif(c == ' '):
+                        pass
+                elif(c == '\r'):
+                        pass
+                elif(c == '\t'):
+                        pass
+                elif(c == '\n'):
+                        self.line += 1
+                else:
 			plox.error(line, "Unexpected character")
 
+        def match(c):
+                if(isAteEnd()):
+                        return False
+                if(self.source[self.current] != c):
+                        return False
+                self.current += 1
+                return True
+        
 	def advance():
 		current += 1
 		return self.source[1:]
+
+        def peek():
+                if(isAtEnd()):
+                        return '\0'
+                return this.source[this.current]
 
 	def addToken(type, literal=None):
 		text = self.source[start:current]
